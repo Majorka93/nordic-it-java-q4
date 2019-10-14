b7934d487796120e11a226109b1fc5380feec5db
Majorka93 <Zaikina1993>
2019-10-14 19:06:14 +0300

    Вычисление сумму четных чисел  диапозоне введенном с клавы и подсчет количества четных чисел которые входят в этот диапозон.

--- src/com/company/HomeWorkLesson5.java
+++ src/com/company/HomeWorkLesson5.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class HomeWorkLesson5 {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Введите диапозон от 0 до ...");
+        var n = scanner.nextInt();
+        var sum = 0;
+        var number = 0;
+        for (var i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                number++;
+                sum += i;
+            }
+        }
+
+        System.out.println("Сумма четных чисел: " + sum);
+        System.out.println("Количество четных чисел: " + number);
+
+    }
 }
+
