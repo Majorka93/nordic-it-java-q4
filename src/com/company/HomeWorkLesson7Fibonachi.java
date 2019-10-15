f64d84b7ab59dee2a6cb786530bd70c168e3981e
Majorka93 <Zaikina1993>
2019-10-15 19:38:39 +0300

    Заполнение списка числами фибоначи.

--- src/com/company/HomeWorkLesson7Fibonachi.java
+++ src/com/company/HomeWorkLesson7Fibonachi.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class HomeWorkLesson7Fibonachi {
     public static void main(String[] args) {
+        var arrayList = new ArrayList<Integer>(30);
+        for (var i = 0; i < 30; i++) {
+            if (i <= 1) {
+                arrayList.add(i);
+            } else {
+                arrayList.add(arrayList.get(i - 1) + arrayList.get(i - 2));
+            }
+        }
+        System.out.println(arrayList);
+
 
     }
 }
